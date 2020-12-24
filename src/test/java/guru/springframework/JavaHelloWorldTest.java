069ec6fdaf0e26c6bea42f3a7cd3291dafc6fd6c
Franck Owen <fxfrancky@yahoo.com>
2020-12-24 13:48:03 +0100

    maven reporting plugin

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaHelloWorldTest {
+
+    @Test
+    public void testGetHello() {
+    }
+}
