c314b0960a821a5e5431fd396e4c3f41d8c3c97b
Franck Owen <fxfrancky@yahoo.com>
2020-12-24 16:00:59 +0100

    Adding integration Test to Report

---
+++ src/test/java/guru/springframework/JavaHelloworldIT.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloworldIT {
+
+    @Test
+    public void myIntegrationTest(){
+        System.out.println("My It run");
+    }
+}
